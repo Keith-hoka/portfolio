4c59bcea5d694652a2e8ac0a380e34c4f5afa9b4
Keith_hoka <keith_d_hau@hotmail.com>
2022-01-19 18:02:36 +1100

    updating blogs

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Blogs from '../components/Blogs';
+import ScrollToTop from '../components/ScrollToTop';
 
 const BlogsPage = () => {
     return (
-        <div>
-            blogs
-        </div>
+        <>
+            <ScrollToTop />
+            <Blogs />
+        </>
     )
 }
 
